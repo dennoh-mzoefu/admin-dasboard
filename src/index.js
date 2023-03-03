23bf13ba778f94afdfabebcc5a08bf765c65b343
Denis Gichana <denisgichana162@gmail.com>
2023-03-03 22:29:21 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { registerLicense } from "@syncfusion/ej2-base";
-
+import { ContextProvider } from "./context/ContextProvider";
 registerLicense(
   "ORg4AjUWIQA/Gnt2VVhiQlFadVlJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdk1jUX5adXxUQmlYV0U="
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
